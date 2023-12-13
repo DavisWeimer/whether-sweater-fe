ae641194331c6d2772b1cffa97a10e3ed96a65fc
Davis Weimer <davisweimer@gmail.com>
2023-12-12 17:29:34 -0700

    feat: Auth config

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { AuthContext } from './context/AuthProvider.jsx'
+import { AuthProvider } from './context/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthContext>
+    <AuthProvider>
       <App />
-    </AuthContext>
+    </AuthProvider>
   </React.StrictMode>,
 )
