b30e3c938e3efd9b891abd804ec2ec29506766f1
Davis Weimer <davisweimer@gmail.com>
2023-12-20 19:46:08 -0800

    fix: apiURL

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,10 +2,7 @@ import axios from 'axios';
 
 const apiUrl = import.meta.env.VITE_API_URL;
 const deployUrl = import.meta.env.VITE_DEPLOY_URL
-const isProduction = window.location.hostname !== 'localhost';
 
-const axiosInstance = axios.create({
+export default axios.create({
     baseURL: apiUrl
 });
-
-export default axiosInstance;
