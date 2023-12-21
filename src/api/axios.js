ffdc700454bbaba3f963a7d46b0088949801db92
Davis Weimer <davisweimer@gmail.com>
2023-12-20 19:39:17 -0800

    feat: window logic

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,6 +2,10 @@ import axios from 'axios';
 
 const apiUrl = import.meta.env.VITE_API_URL;
 const deployUrl = import.meta.env.VITE_DEPLOY_URL
-export default axios.create({
-    baseURL: deployUrl
+const isProduction = window.location.hostname !== 'localhost';
+
+const axiosInstance = axios.create({
+    baseURL: isProduction ? deployUrl : apiUrl
 });
+
+export default axiosInstance;
