5d0aa31cd95c743b2effdea979c126c975422a3a
Davis Weimer <davisweimer@gmail.com>
2023-12-20 18:43:46 -0800

    feat: deployUrl added

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const apiUrl = import.meta.env.VITE_API_URL;
+const deployUrl = import.meta.env.VITE_DEPLOY_URL
 export default axios.create({
-    baseURL: apiUrl
+    baseURL: deployUrl
 });
