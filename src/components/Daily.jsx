bbbfceefcc0c59c4ea6819ef4c702977923778f1
Davis Weimer <davisweimer@gmail.com>
2023-12-26 16:38:22 -0700

    feat: Daily Card props passed

--- src/components/Daily.jsx
+++ src/components/Daily.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import Sunny from '../assets/Sunny 2.svg'
+import TempUp from '../assets/Up Arrow.svg'
+import TempDown from '../assets/Down Arrow.svg'
 
-function Daily(props) {
-  console.log(props)
+function Daily({ weatherData }) {
+  const lastUpdated = weatherData.current_weather?.last_updated;
+  const lastUpdatedTime = lastUpdated.split(' ')[1];
+
+  console.log("weather props", weatherData)
   return (
     <div className="min-h-[20em] font-dm-sans bg-turq-gradient-to-t shadow-md rounded-2xl border-2 border-turquiose justify-evenly items-center inline-flex">
       <div className="p-4 flex-col justify-start items-center gap-3 inline-flex">
         <div className="self-stretch px-4 py-2.5 border-2 border-turquiose rounded-full justify-start items-center gap-3 inline-flex">
-          <div className="text-offWhite text-sm font-bold font-dm-sans">Partly Cloudy</div>
+          <div className="text-offWhite text-sm font-bold font-dm-sans">{weatherData.current_weather?.condition}</div>
             
-          <img src={Sunny} />
+          <img src={weatherData.current_weather?.icon} className='w-[32px]' />
         </div>
             <div className="pr-6 py-3 text-sm border-r-2 border-turquiose justify-between items-center inline-flex">
                 <div className="flex-col justify-center items-left gap-2.5 text-offWhite inline-flex">
@@ -20,26 +25,31 @@ function Daily(props) {
                     <div>Sunset:</div>
                 </div>
                 <div className="flex-col justify-center items-center gap-2.5 text-offWhite font-dm-sans-bold inline-flex">
-                    <div>45°</div>
-                    <div>30%</div>
-                    <div>20 mil</div>
-                    <div>6:23 AM</div>
-                    <div>8:09 PM</div>
+                    <div>{weatherData.current_weather?.feels_like}°</div>
+                    <div>{weatherData.current_weather?.humidity}</div>
+                    <div>{weatherData.current_weather?.visibility} miles</div>
+                    <div>{weatherData.daily_weather?.[0].sunrise}</div>
+                    <div>{weatherData.daily_weather?.[0].sunset}</div>
                 </div>
             </div>
         </div>
         <div className="py-4 pr-4 rounded-full flex-col justify-start items-center gap-3 text-offWhite inline-flex">
             <div className="text-2xl lg:text-4xl ">Denver, CO</div>
-            <div className="text-xl font-normal ">2:21 PM, April 18</div>
-            <div className="text-8xl font-normal ">65°</div>
+            <div className="text-xl font-normal ">Updated: {lastUpdatedTime}</div>
+            <div className="text-6xl md:text-8xl font-normal ">{weatherData.current_weather?.temperature}°</div>
             <div className="justify-start text-lg items-center gap-4 inline-flex">
-                <div><span className='text-sm'>High:</span><span className=''> 68°</span></div>
-                <div><span className='text-sm'>Low:</span><span className=''> 61°</span></div>
+            <div className="flex items-center gap-1"> 
+              <img src={TempUp} className='w-[24px]'/>
+              <span>{weatherData.daily_weather?.[0].max_temp}°</span>
             </div>
+            <div className="flex items-center gap-1"> 
+              <img src={TempDown} className='w-[24px]'/>
+              <span>{weatherData.daily_weather?.[0].min_temp}°</span>
+            </div>
+          </div>
         </div>
     </div>
   )
 }
 
 export default Daily;
-
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -17,13 +17,13 @@ const Dashboard = () => {
   useEffect(() => {
     const fetchWeather = async () => {
       try {
-        // const response = await axios.get("/api/v0/forecast", {
-        //   params: { location: "denver,co" },
-        //   headers: { Authorization: auth.bearerToken },
-        //   withCredentials: true
-        // });
-        // setWeather(response);
-        // console.log(response)
+        const response = await axios.get("/api/v0/forecast", {
+          params: { location: "denver,co" },
+          headers: { Authorization: auth.bearerToken },
+          withCredentials: true
+        });
+        setWeather(response.data.data.attributes); 
+        console.log("weather data", weather);
       } catch (err) {
         if (!err?.response) {
           setErrMsg('No Server Response');
@@ -33,14 +33,8 @@ const Dashboard = () => {
       }
     };
 
-    // fetchWeather();
-
-    const intervalId = setInterval(() => {
-      fetchWeather();
-    }, 60000);
-
-    return () => clearInterval(intervalId); 
-  }, [auth.bearerToken]);
+    fetchWeather();
+  }, [auth.bearerToken]); 
 
   const handleLogout = async () => {
     try {
@@ -75,7 +69,8 @@ const Dashboard = () => {
         <RoadTrips
           authCreds={auth} 
         />
-        <Forecast 
+        <Forecast
+          weatherData={weather}
           className="min-h-[30em] row-span-2 md:col-span-2 bg-turq-gradient-to-b shadow-md rounded-2xl border-2 border-turquiose"
         />
       </section>
